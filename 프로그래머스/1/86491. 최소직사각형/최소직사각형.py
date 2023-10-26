c8db9456b73d43fc0863dd3ebb62cb40a4087231
palesaltedcaramel <107484982+palesaltedcaramel@users.noreply.github.com>
2023-10-27 00:06:30 +0900

    [level 1] Title: 최소직사각형, Time: 0.80 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,15 @@
+def solution(sizes):
+    w, h = 0, 0
+    for i, j in sizes: 
+        if j > i:
+            i,j = j,i
+            if i > w: 
+                w = i 
+            if j > h : 
+                h = j 
+        else:
+            if i > w: 
+                w = i 
+            if j > h : 
+                h = j 
+    return w*h
